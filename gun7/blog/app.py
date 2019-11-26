840ea2f9aacc096453bcb357bb21cb63e8f29b0b
Furkan KARAGÖZ <furkannkaragozz@gmail.com>
2019-11-26 17:35:49 +0300

    Blog Anasayfasi Yayinlanma Tarihi Siralamasi

--- gun7/blog/app.py
+++ gun7/blog/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, session
 from gun7.blog.views.admin.BlogController import blog
 from gun7.blog.views.admin.UyeController import uyeler
 from gun7.blog.views.admin.GirisController import giris
+from gun7.blog.views.AnasayfaController import anasayfa
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(blog)
 app.register_blueprint(uyeler)
 app.register_blueprint(giris)
+app.register_blueprint(anasayfa)
 
 
 if __name__ == "__main__":
--- gun7/blog/views/AnasayfaController.py
+++ gun7/blog/views/AnasayfaController.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template
-
-blog = Blueprint('anasayfa',__name__,
+from gun7.blog.utils.Db import Db
+anasayfa = Blueprint('anasayfa',__name__,
                  url_prefix="/anasayfa")
 
 
-@blog.route("/")
+def sort(val):
+    return val[3]
+
+
+@anasayfa.route("/")
 def index():
-    return render_template("/index.html")
+
+    databse = Db()
+    sql = """ select * from "BlogYazisi" """
+    data = databse.read_data(sql);
+    data.sort(key=sort,reverse = True)
+
+    return render_template("anasayfa/index.html",data=data)

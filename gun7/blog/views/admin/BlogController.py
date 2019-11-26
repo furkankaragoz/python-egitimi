901e2ea38d4d0bbbe458746ba1509b4672737df3
Furkan KARAGÖZ <furkannkaragozz@gmail.com>
2019-11-26 16:52:51 +0300

    BlogYazisi Operasyonlari

--- gun7/blog/views/admin/BlogController.py
+++ gun7/blog/views/admin/BlogController.py
@@ -1,9 +1,60 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template,request,redirect
+from gun7.blog.utils.Db import Db
 blog = Blueprint('blog',__name__,
                  url_prefix="/admin/blog")
 
 
 @blog.route("/index")
 def index():
-    return render_template("admin/blog/index.html")
+
+    databse = Db()
+    sql = """ select * from "BlogYazisi" """
+    data = databse.read_data(sql);
+
+    return render_template("admin/blog/index.html",data=data)
+
+
+@blog.route('/ekle', defaults={'id': None})
+@blog.route('/ekle/<string:id>')
+def ekle(id):
+
+    if id is not None:
+        databse = Db()
+        sql = """ select * from "BlogYazisi" where "Id" = %s """
+        data = databse.read_first_data(sql,(id,))
+        print(data)
+
+        return render_template("admin/blog/ekle.html",data=data)
+
+    return render_template("admin/blog/ekle.html",data=None)
+
+@blog.route('/save',methods=["POST"])
+def save():
+    Baslik = request.form.get("Baslik")
+    id = request.form.get("Id")
+    Icerik = request.form.get("Icerik")
+    YayinTarihi = request.form.get("YayinTarihi")
+
+
+    database = Db()
+
+
+    sql  = ""
+    if id is None:
+        sql = """ insert into "BlogYazisi" ("Baslik","Icerik","YayinTarihi") values (%s,%s,%s) """
+        database.execute(sql,(Baslik,Icerik,YayinTarihi))
+    else:
+        sql = """ update "BlogYazisi" set "Baslik"=%s, "Icerik"=%s,"YayinTarihi"=%s """
+        params= (Baslik,Icerik,YayinTarihi,id)
+
+        sql += """ where "Id" = %s"""
+
+        database.execute(sql,params)
+
+    return redirect("/admin/blog/index")
+@blog.route('/sil/<string:id>')
+def sil(id):
+    database = Db()
+    sql = """ delete from "BlogYazisi" where "Id"=%s """
+    database.execute(sql, (id,))
+    return redirect("/admin/blog/index")
